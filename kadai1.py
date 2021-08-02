67b8e83ff481a96dcb10622e9257991b0b6dc891
mycani1996 <mycani1996@gmail.com>
2021-08-02 22:23:13 +0900

    modify first

--- kadai1.py
+++ kadai1.py
@@ -1,23 +1,31 @@
 import csv
 
-path = './data/source.csv'
-csv_header = ['No','Name']
+DATA_PATH = './data/source.csv'
 
-def check_char(char_in):
-    with open(path, mode='r') as char_file:
-        char_list = [row for row in csv.reader(char_file)][0]
-        # print(char_list)
-        if set([char_in]) <= set(char_list):
-            print('"',char_in,'"は追加済みです')
+def check_name(name):
+    with open(DATA_PATH, mode='r') as file:
+        list = [row for row in csv.reader(file)][0]
+        if set([name]) <= set(list):
+            print(f'{name}は追加済みです')
         else:
-            print('存在しません。"',char_in,'"を追加します')
-            with open(path, mode='w') as char_file_w:
-                char_list.append(char_in)
-                writer = csv.writer(char_file_w)
-                writer.writerow(char_list)
-                # print(char_list)
-char_name = input("好きなキャラクター名を入力してください（'fin'で終了）：")
-while char_name != "fin":
-    check_char(char_name)
-    char_name = input("好きなキャラクター名を入力してください（'fin'で終了）：")
-print("終了します")
+            print(f'存在しません。{name}を追加します')
+            with open(DATA_PATH, mode='w') as file_w:
+                list.append(name)
+                writer = csv.writer(file_w)
+                writer.writerow(list)
+
+def main():
+    charactor_name = input("好きなキャラクター名を入力してください（'fin'で終了）：")
+    if charactor_name != "fin":
+        check_name(charactor_name)
+        return True
+    else:
+        print("終了します")
+        return False
+
+if __name__ == "__main__":
+    flg = True
+    while flg:
+        flg = main()
+    
+
